38844cf5c9e63b204c40f60856d22624486e469b
DESKTOP-OAKDG8P\張澤瑜 <a106082016@mail.shu.edu.tw>
2018-03-16 04:35:33 +0800

    基本功能

--- BmiHomework/BmiHomework/Program.cs
+++ BmiHomework/BmiHomework/Program.cs
@@ -10,6 +10,14 @@ namespace BmiHomework
     {
         static void Main(string[] args)
         {
+            
+            Console.Write("請輸入您的身高(公分)");
+            float height = float.Parse(Console.ReadLine())/100;
+            Console.Write("請輸入您的體重(公斤)");
+            float weight = float.Parse(Console.ReadLine());
+            Console.Write("您的BMI為"+weight/Math.Pow(height,2));
+
+            Console.ReadLine();
         }
     }
 }
