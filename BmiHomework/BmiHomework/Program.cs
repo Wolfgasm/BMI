adb617d0b61c5fe75463b05c434c92187d1bd91c
DESKTOP-OAKDG8P\張澤瑜 <a106082016@mail.shu.edu.tw>
2018-03-16 04:41:32 +0800

    增進程式碼可讀性

--- BmiHomework/BmiHomework/Program.cs
+++ BmiHomework/BmiHomework/Program.cs
@@ -10,13 +10,17 @@ namespace BmiHomework
     {
         static void Main(string[] args)
         {
-            
+            // 請使用者輸入身高並且自動轉換為公尺單位
             Console.Write("請輸入您的身高(公分)");
-            float height = float.Parse(Console.ReadLine())/100;
+            float height = float.Parse(Console.ReadLine()) / 100;
+            // 請使用者輸入體重
             Console.Write("請輸入您的體重(公斤)");
             float weight = float.Parse(Console.ReadLine());
-            Console.Write("您的BMI為"+weight/Math.Pow(height,2));
 
+            // 計算BMI及顯示結果
+            Console.Write("您的BMI為" + weight / Math.Pow(height, 2));
+
+            // 暫停畫面用
             Console.ReadLine();
         }
     }
